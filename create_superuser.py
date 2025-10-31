48deebcdb3c2d883e3599581b655c5d775787fe0
Vladimir <vovochka.ackord@gmail.com>
2025-10-31 21:42:33 +0300

    Описание изменений

---
+++ create_superuser.py
@@ -0,0 +1,25 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "YandexCurrier.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+SUPERUSER_USERNAME = os.environ.get("DJANGO_SUPERUSER_USERNAME",)
+SUPERUSER_EMAIL = os.environ.get("DJANGO_SUPERUSER_EMAIL",)
+SUPERUSER_PASSWORD = os.environ.get("DJANGO_SUPERUSER_PASSWORD",)
+
+if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
+    print(f"Создаем суперпользователя {SUPERUSER_USERNAME}...")
+    User.objects.create_superuser(
+        username=SUPERUSER_USERNAME,
+        email=SUPERUSER_EMAIL,
+        password=SUPERUSER_PASSWORD
+    )
+    print("Суперпользователь создан!")
+else:
+    print("Суперпользователь уже существует.")
